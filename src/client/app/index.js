073443ac700de402cc98576d354deb227769a0f9
orizens <farhioren@gmail.com>
2015-07-21 17:53:26 +0300

    added user story with repeat

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,1 +1,30 @@
 //define Vue.js app
+var data = { 
+	story: {
+		feature: 'eggroll editor',
+		scenarios: [
+			{
+				name: 'add new one',
+				given: ['i logged in with my github account', 'and i authorized eggroll'],
+				when: ['i save a story'],
+				then: ['i should get feature files in my repo']
+			},
+			{
+				name: 'add new one',
+				given: ['i logged in with my github account', 'and i authorized eggroll'],
+				when: ['i save a story'],
+				then: ['i should get feature files in my repo']
+			}
+		]
+	}
+};
+
+start();
+
+//////////////
+function start () {
+	return new Vue({
+		el: '#app',
+		data: data
+	});
+}
--- src/client/common/user-story.js
+++ src/client/common/user-story.js
@@ -1,4 +1,30 @@
+var UserStoryFactory = {
+	scenario: function () {
+		return {
+			name: '',
+			given: [''],
+			when: [''],
+			then: ['']
+		}
+	},
+	given: function () {
+		return '';
+	}
+}
 Vue.component('user-story', {
     props: ['story'],
-    template: '#user-story-template'
+    template: '#user-story',
+    methods: {
+    	addScenario: function () {
+    		this.story.scenarios.push(UserStoryFactory.scenario());
+    		setTimeout(function () {
+	    		componentHandler.upgradeAllRegistered();
+    		}, 1000)
+    	},
+
+    	addGiven: function (scenario) {
+    		scenario.given.push(UserStoryFactory.given());
+    		componentHandler.upgradeAllRegistered();
+    	}
+    }
 });
